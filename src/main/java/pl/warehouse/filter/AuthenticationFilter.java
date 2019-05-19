0e47dca7e68ec5536e6c7a474dbbd0cde141cf9c
Szymon Patora <szymonopatora@gmail.com>
2019-05-19 14:55:54 +0200

    AuthenticationFilter added

--- src/main/java/pl/warehouse/filter/AuthenticationFilter.java
+++ src/main/java/pl/warehouse/filter/AuthenticationFilter.java
@@ -1,4 +1,22 @@
 package pl.warehouse.filter;
 
-public class AuthenticationFilter {
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "authenticationFilter", servletNames = {"ToolList"})
+public class AuthenticationFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+
+        if (req.getSession().getAttribute("userName") == null && !req.getRequestURI().endsWith("/login")) {
+            res.sendRedirect("/login");
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
 }
